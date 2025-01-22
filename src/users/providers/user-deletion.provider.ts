671381c4b27baf878875785004e161b8b7bf361c
Omer-Mazig <114055368+Omer-Mazig@users.noreply.github.com>
2025-01-22 17:40:44 +0200

    refactor delete user to get the user instead of user id

--- src/users/providers/user-deletion.provider.ts
+++ src/users/providers/user-deletion.provider.ts
@@ -14,25 +14,16 @@ import { BoardMember } from 'src/boards/entities/board-member.entity';
 export class UserDeletionProvider {
   constructor(private readonly dataSource: DataSource) {}
 
-  async deleteUser(userId: string): Promise<void> {
+  async deleteUser(user: User): Promise<void> {
     const queryRunner = this.dataSource.createQueryRunner();
 
     await queryRunner.connect();
     await queryRunner.startTransaction();
 
     try {
-      const user = await this.fetchUser(queryRunner, userId);
-
       // Handle workspaces and boards
       await this.handleUserWorkspaces(queryRunner, user);
       await this.handleUserBoards(queryRunner, user);
-
-      // Remove memberships
-      await this.removeUserMemberships(queryRunner, user);
-
-      // Check for orphaned workspaces and boards
-      // await this.cleanupOrphanedWorkspaces(queryRunner);
-      // await this.cleanupOrphanedBoards(queryRunner);
 
       // Delete the user
       await queryRunner.manager.getRepository(User).remove(user);
@@ -47,21 +38,6 @@ export class UserDeletionProvider {
     } finally {
       await queryRunner.release();
     }
-  }
-
-  private async fetchUser(
-    queryRunner: QueryRunner,
-    userId: string,
-  ): Promise<User> {
-    const user = await queryRunner.manager.getRepository(User).findOne({
-      where: { id: userId },
-    });
-
-    if (!user) {
-      throw new NotFoundException(`User with ID ${userId} not found.`);
-    }
-
-    return user;
   }
 
   private async handleUserWorkspaces(
@@ -155,7 +131,9 @@ export class UserDeletionProvider {
       if (adminCount <= 1) {
         if (members.length === 1) {
           // Delete the board if this user is the only member
-          await queryRunner.manager.getRepository(Board).delete(board);
+          await queryRunner.manager
+            .getRepository(Board)
+            .delete({ id: board.id });
         } else {
           // Promote the oldest member to admin
           const newAdmin = members.find((m) => m.user.id !== user.id);
@@ -164,45 +142,6 @@ export class UserDeletionProvider {
             await queryRunner.manager.getRepository(BoardMember).save(newAdmin);
           }
         }
-      }
-    }
-  }
-
-  private async removeUserMemberships(
-    queryRunner: QueryRunner,
-    user: User,
-  ): Promise<void> {
-    await queryRunner.manager.getRepository(WorkspaceMember).delete({ user });
-    await queryRunner.manager.getRepository(BoardMember).delete({ user });
-  }
-
-  // TODO: query only user related
-  private async cleanupOrphanedWorkspaces(
-    queryRunner: QueryRunner,
-  ): Promise<void> {
-    const orphanedWorkspaces = await queryRunner.manager
-      .getRepository(Workspace)
-      .find({
-        relations: ['members'],
-      });
-
-    for (const workspace of orphanedWorkspaces) {
-      if (workspace.members.length === 0) {
-        await queryRunner.manager.getRepository(Board).delete({ workspace });
-        await queryRunner.manager.getRepository(Workspace).delete(workspace.id);
-      }
-    }
-  }
-
-  // TODO: query only user related
-  private async cleanupOrphanedBoards(queryRunner: QueryRunner): Promise<void> {
-    const orphanedBoards = await queryRunner.manager.getRepository(Board).find({
-      relations: ['members'],
-    });
-
-    for (const board of orphanedBoards) {
-      if (board.members.length === 0) {
-        await queryRunner.manager.getRepository(Board).delete(board.id);
       }
     }
   }
